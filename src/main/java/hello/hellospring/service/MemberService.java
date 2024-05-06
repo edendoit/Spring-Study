42b33d07ca48139c8ea4a82ad9d09e8fa2a5f2f6
mhpark <happyrichbb@gmail.com>
2024-05-07 06:03:25 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,15 +24,17 @@ public class MemberService {
      *
      */
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 X
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
-        // 중복 회원 검증
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("join = "+ timeMs + "ms");
+        }
+
 
     }
 
